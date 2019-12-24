d84c8d549fac21c57dc0a869e5f59dd4d6cf69b1
yeshan333 <1329441308@qq.com>
2019-12-24 19:19:11 +0800

    404 handler, users api controler function refactor

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import json
 import sqlite3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def index():
     return 'Nothing'
 
+# get api information
 @app.route('/api/v1/info')
 def home_index():
     conn = sqlite3.connect('app.db')
@@ -28,26 +29,32 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
-@app.route('/api/v1/users', methods=['GET'])
-def get_users():
-    return list_users()
+# get user information
+@app.route('/api/v1/users/<int:user_id>', methods=['GET'])
+def get_users(user_id):
+    return list_users(user_id)
 
-def list_users():
+def list_users(user_id):
     conn = sqlite3.connect('app.db')
     print('Open Database Sucessful!')
     api_list = []
-    cursor = conn.execute('select username, full_name, emailid, password, id from users')
-    for row in cursor:
-        a_dict = {}
-        a_dict['username'] = row[0]
-        a_dict['full_name'] = row[1]
-        a_dict['email'] = row[2]
-        a_dict['password'] = row[3]
-        a_dict['id'] = row[4]
-        api_list.append(a_dict)
+    # cursor = conn.execute('select username, full_name, emailid, password, id from users')
+    cursor = conn.execute('select * from users where id=?',(user_id, ))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        user = {}
+        user['username'] = data[0][0]
+        user['email'] = data[0][1]
+        user['password'] = data[0][2]
+        user['full_name'] = data[0][3]
+        user['id'] = data[0][4]
+        api_list.append(user)
     conn.close()
     return jsonify({'user_list':api_list})
 
+@app.errorhandler(404)
+def resource_not_found(error):
+    return make_response({'error':'Resource not found!'}, 404)
 
 if __name__ == '__main__':
     app.run('127.0.0.1', port=5000, debug=True)
